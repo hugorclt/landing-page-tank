1d4495a0871ab42df7b960f30a46a37b1d0c830d
Hugo Récolet <hugorecolet@gmail.com>
2023-06-15 19:22:40 +0200

    [UPDATE]fixed some details about padding

--- app/components/HeroBanner/HeroBanner.tsx
+++ app/components/HeroBanner/HeroBanner.tsx
@@ -3,19 +3,23 @@ import styles from "./page.module.css";
 export default function HeroBanner() {
   return (
     <section className={styles.heroContainer}>
-        <div className={styles.heroTitle}>
-          <h1>Plateforme de recrutement pour le médical</h1>
-        </div>
+      <div className={styles.heroTitle}>
+        <h1>Plateforme de recrutement pour le médical</h1>
+      </div>
       <div className={styles.heroDesc}>
         Facilitez vos recrutements médicaux avec intelligence, dans un esprit
         friendly !
       </div>
       <div className={styles.heroButtonContainer}>
-        <div className={`${styles.heroMainButton} button-large`}>
-          <a href="https://mmra.re/purchasetiles">Recrutement</a>
+        <div className={`${styles.heroMainButton}`}>
+          <a href="https://mmra.re/purchasetiles">
+            <div className={styles.buttonText}>Recrutement</div>
+          </a>
         </div>
         <div className={styles.secondaryButton}>
-          <a href="#">Interim</a>
+          <a href="#">
+            <div className={styles.buttonText}>Interim</div>
+          </a>
         </div>
       </div>
     </section>
