2d6af4bdd9f0211ef1724950bff57daabb326764
Hugo <hugo@hugos-mbp.home>
2023-10-06 10:51:44 +0200

    [UP]news

--- utils/fetchMission.ts
+++ utils/fetchMission.ts
@@ -1,5 +1,5 @@
 export default async function getMissions() {
-	const res = await fetch('https://tankserverdeploy-tpg5dhuatq-od.a.run.app/api/annoucement/useCaseAnnoucement');
+	const res = await fetch('http://localhost:3000/api/annoucement/useCaseAnnoucement');
 
 	if (!res.ok) throw new Error('Failed to fetch latest missions');
 
